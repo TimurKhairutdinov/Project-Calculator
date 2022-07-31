07b942d812d20dfbb0897ebb609930bdee66cb00
Timur.Khairutdinov <t1m.1h@yandex.ru>
2022-07-31 12:06:06 +0300

    edit log

--- calc/log.py
+++ calc/log.py
@@ -1,8 +1,8 @@
-
 import datetime
-now = datetime.datetime.now()
 
 
 def log(text):
-    with open('log.txt', 'a', encoding='utf-8') as log:
+
+    now = datetime.datetime.now()
+    with open('log.txt', 'a', encoding='utf-16') as log:
         log.writelines(f'\n{now} : {text}')
