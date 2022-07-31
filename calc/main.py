21964d783e5ee801a8ccff7d35ea4bf04433ec6c
Sergey Lebedev <sergeylebedev87@gmail.com>
2022-07-31 17:17:44 +0300

    Подправил end_prog(Он теперь вообще не нужен), main(закоментированное можно удалить), out, main(закоментированное можно удалить)

--- calc/main.py
+++ calc/main.py
@@ -7,9 +7,9 @@ from init_status import status_prog
 
 while status_prog != False:
     menu.menu()
-    s.click()
+    #s.click()
     l.loging(f'Ввод пользователем {s.text_from_user}')
-    menu.mn_select(s.text_from_user)
+    #menu.mn_select(s.text_from_user)
     status_prog = menu.stop_prog()
 
-    
+
