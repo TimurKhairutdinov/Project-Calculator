f8f2acbd6a6fdcd6182abbc2db458e6736ac1d57
Timur.Khairutdinov <t1m.1h@yandex.ru>
2022-08-01 14:43:51 +0300

    merge from Sergey

--- calc/main.py
+++ calc/main.py
@@ -5,11 +5,9 @@ import log as l
 from init_status import status_prog
 
 
-while status_prog != False:
+while status_prog:
     menu.menu()
-    #s.click()
     l.loging(f'Ввод пользователем {s.text_from_user}')
-    #menu.mn_select(s.text_from_user)
     status_prog = menu.stop_prog()
 
 
