8c873d6a00af64fe4844da3f4b8bd6b02de45a35
Timur.Khairutdinov <t1m.1h@yandex.ru>
2022-08-03 22:35:47 +0300

    Edit vvod function

--- calc/vvod.py
+++ calc/vvod.py
@@ -2,18 +2,22 @@ from secrets import choice
 import easygui as gui
 import gui as g
 
-a = 0
-b = 0
-operation = ''
-ch = ["+", "-", "*"]
-
-# action = buttonbox("Choose:", choices = ch)
-
 
 def vvod():
-    global a, b, operation
-    box = gui.multenterbox(g.msg, g.title,
-                           fields=['Введите первое число','Введите второе число: '], choices = [ch])
+    global a, b, op
+    box = gui.multenterbox(g.msg,
+                           g.title,
+                           fields=[
+                               'Введите первое число',
+                               'Арифметическая операция',
+                               'Введите второе число: '
+                           ])
     a = box[0]
-    b = box[1]
-    operation = box[2]
+    op = str(box[1])
+    b = box[2]
+    print(op)
+    print(type(op))
+    if op != '+' and op != '-' and op != '*' and op != '/' and op != '//':
+        gui.textbox(f'Неккоректный ввод Арифметической операции {op}')
+        vvod()
+    
